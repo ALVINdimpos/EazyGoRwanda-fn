b3182eb1cff360231a26b25892a74ea9f91dd32d
“ALVINdimpos” <alvindimpos@gmail.com>
2024-03-07 15:14:33 +0200

    fix: Herro section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
   return (
     <section className=''>
       <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-[100vh]'>
-        <div className='mr-auto place-self-center lg:col-span-7 mt-[-40px]'>
+        <div className='mr-auto place-self-center lg:col-span-7 mt-[20px]'>
           <h1 className='max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-floor'>
             Save Time, Save Money, Safe Rides: Your Journey, Your Way!
           </h1>
@@ -44,7 +44,7 @@ const HeroSection = () => {
           </Link>
         </div>
         <div className='hidden lg:mt-0 lg:col-span-5 lg:flex'>
-          <img src={Hero} alt='mockup' className='h-[350px] ' />
+          <img src={Hero} alt='mockup' className='h-[400px] ' />
         </div>
 
         {/*Search Navigation  */}
--- src/components/Home/SearchNav.jsx
+++ src/components/Home/SearchNav.jsx
@@ -1,28 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { MapPin } from 'lucide-react';
 import { DatePicker } from 'antd';
 
 function SearchNav() {
   const handleSubmit = () => {};
+
   return (
-    <div className='w-full h-40 col-span-full bg-transparent my-10'>
-      <form onSubmit={handleSubmit} className='flex items-center gap-12 w-full h-full pr-8 pl-3 bg-primary rounded-lg'>
-        <div className='w-40'>
+    <div className='w-full h-40 my-10 bg-transparent col-span-full'>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col items-center w-full h-full gap-4 pl-3 pr-8 rounded-lg lg:flex-row lg:gap-12 bg-primary'
+      >
+        <div className='w-full lg:w-60'>
           <p className='text-2xl'>Need a ride?</p>
           <p className='text-white'>Find a ride and carpool anywhere in Rwanda.</p>
         </div>
-        <div className='relative flex items-center w-2/12'>
+        <div className='relative flex items-center w-full lg:w-2/12'>
           <MapPin className='absolute ml-1' />
-          <input type='text' placeholder='From' className='h-20 w-full pl-12 rounded-md' />
+          <input type='text' placeholder='From' className='w-full h-20 pl-12 rounded-md' />
         </div>
-        <div className='relative flex items-center w-2/12'>
+        <div className='relative flex items-center w-full mt-4 lg:w-2/12 lg:mt-0'>
           <MapPin className='absolute ml-1' />
-          <input type='text' placeholder='To' className='h-20 w-full pl-12 rounded-md' />
+          <input type='text' placeholder='To' className='w-full h-20 pl-12 rounded-md' />
         </div>
 
-        <DatePicker placeholder='Leaving time(optional)' className='h-20 w-3/12 pl-12 rounded-md' />
+        <DatePicker
+          placeholder='Leaving time(optional)'
+          className='w-full h-20 pl-12 mt-4 rounded-md lg:w-3/12 lg:mt-0'
+        />
 
-        <button type='submit' className='h-20 w-3/12 p-2 lg:bg-black text-white rounded-md'>
+        <button type='submit' className='w-full h-20 p-2 mt-4 text-white rounded-md lg:w-3/12 lg:bg-black lg:mt-0'>
           Find a ride
         </button>
       </form>
