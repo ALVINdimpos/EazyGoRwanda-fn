2e43b9dee37563a12db6140480c4c87eaebae6db
Bryan Murasira <bryanmurasira@gmail.com>
2024-03-07 11:20:43 +0200

    feat: added search bar navigation

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import { Navbar } from '../components';
 import HeroSection from '../components/HeroSection';
+import SearchNav from '../components/Home/SearchNav';
+
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      {/* <SearchNav /> */}
     </div>
   );
 };
