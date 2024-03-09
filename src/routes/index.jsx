66d3733263b8f432c0618eae2ffc4ed5691a2af9
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-03-09 20:57:34 +0300

    Created HowItWorks page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,10 @@
+import HowItWorks from '../components/HowItWorks/HowItWorks';
 import { HomePage, NotFoundPage, UnAuthorizedPage, FindRidePage, TravelPage } from '../pages';
 
 // Public Routes
 const publicRoutes = [
   { path: '/', element: <HomePage /> },
+  { path: 'how-it-works', element: <HowItWorks /> },
   { path: '/unauthorized', element: <UnAuthorizedPage /> },
   { path: '*', element: <NotFoundPage /> },
   { path: '/find-ride', element: <FindRidePage /> },
