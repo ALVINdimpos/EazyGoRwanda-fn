cba8562a949d0944f231f444ebf0f9a395108e3c
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-03-12 22:34:21 +0300

    ft: Implement tab navigation and responsiveness in HowItWorks

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Navbar } from '../components';
+import HowItWorks from '../components/HowItWorksSection/HowItWorks';
 import HeroSection from '../components/HeroSection';
 import Footer from './Footer';
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
+      <HowItWorks />
       <Footer />
     </div>
   );
