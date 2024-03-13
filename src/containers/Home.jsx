028337088bf46b9cf0a5ec8fe70136b07276d718
Bryan Murasira <bryanmurasira@gmail.com>
2024-03-13 17:01:09 +0200

    feat: added EasyGo Benefits section

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Navbar } from '../components';
 import HeroSection from '../components/HeroSection';
+import EasyGoBenefits from '../components/EasyGoBenefits';
 import Footer from './Footer';
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <EasyGoBenefits />
       <Footer />
     </div>
   );
